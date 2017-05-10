0caeb040f4cdf6dcec8c80590cd121bb189637d8
andres <andreslago5@hotmail.com>
2017-05-09 23:58:19 -0300

    Added function for interactive plot

--- launchers/plot.py
+++ launchers/plot.py
@@ -108,6 +108,7 @@ def main(pklPath,real=False):
     for i in range(0, len(set(df['label']))):
         nc = generate_new_color(colorList, pastel_factor=0.9)
         colorList.append(nc)
+    print colorList
     for i in range(len(colorList)):
         colorList[i] = [colorList[i][0] * 255, colorList[i][1] * 255, colorList[i][2] * 255]
     colorList =  ['#%02x%02x%02x' % tuple(x) for x in colorList]
