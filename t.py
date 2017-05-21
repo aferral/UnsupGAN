ef6a63aa849f4479de71323e5fb034b4d220b782
andres <andreslago5@hotmail.com>
2017-05-20 22:26:02 -0300

    Clasification exp ready and also table generator from txt files

--- t.py
+++ t.py
@@ -1,95 +1,57 @@
 from pandas import ExcelWriter
-
 import pandas as pd
-
 import os
 
 
 
 fbase = 'imagenesTest'
-
 listaFolders = os.listdir(fbase)
-
 listaFolders.remove('test')
-
 listaFolders.remove('old')
 
 
-
 for folder in listaFolders: #Tabla
-
 	dataset = folder
-
 	listaFiles = os.listdir(os.path.join(fbase,folder))
-
+	writer = ExcelWriter(os.path.join(fbase, dataset + '.xls'))
+	hoja = 0
 	data = {}
-
 	for file in listaFiles:
-
 		if file.split('.')[-1] == 'txt':
 
 			dataTransform = file.split()[2]
+			methodName = " ".join(file.split()[3:]).replace('.txt','')
 
-			methodName = " ".join(file.split()[3:])
+			temp = {}
 
-			
-
-			
-
-			with open(os.path.join(fbase,folder,file),'r') as f:	
-
-				texto = f.read() 
-
+			with open(os.path.join(fbase,folder,file),'r') as f:
+				texto = f.read()
 				ultimas = texto.split('\n')[-7:]
-
 			print ultimas
 
 			ari = float(ultimas[0].split()[-1])
-			dtranf[methodName+'ari'] = ari
+			temp['ari'] = ari
 
 			nmi = float(ultimas[1].split()[-1])
-			dtranf[methodName+'nmi'] = nmi
+			temp['nmi'] = nmi
 
 			pred = float(ultimas[2].split()[-1])
-			dtranf[methodName+'pred'] = pred
+			temp['pred'] = pred
 
 			meanCscore  = float(ultimas[3].split()[-1])
-			dtranf[methodName+'meanCscore'] = meanCscore
+			temp['meanCscore'] = meanCscore
 
-			stdCscore  = float(ultimas[4].split()[-1]) 
-			dtranf[methodName+'stdCscore'] = stdCscore
+			stdCscore  = float(ultimas[4].split()[-1])
+			temp['stdCscore'] = stdCscore
 
-			pcavar  = (ultimas[4].split('variance')[-1])
-			dtranf[methodName+'stdCscore'] = stdCscore
+			pcavar  = sum(map(float,(ultimas[5].split('variance[')[-1])[:-1].split()))
+			temp['pcavar'] = pcavar
 
-			
+			data[str(dataTransform) + "_" + str(methodName)] = temp
 
-			print (ari,nmi,pred,meanCscore,stdCscore,pcavar)
-
-			if data.has_key(dataTransform):
-
-				dtranf = data[dataTransform]
-
-				dtranf[methodName] = (ari,nmi,pred,meanCscore,stdCscore,pcavar)
-
-			else:
-
-				data[dataTransform] = {}
-
-				dtranf = data[dataTransform]
-
-				dtranf[methodName] = (ari,nmi,pred,meanCscore,stdCscore,pcavar)
-
-				
-
-			
-
-	df = pd.DataFrame(data)
-
-	writer = ExcelWriter(os.path.join(fbase,dataset+'.xls'))
-
-	df.to_excel(writer,'hoja1')
-
+	df = pd.DataFrame(data).transpose()
+	df.to_excel(writer,'hoja0')
+	hoja+=1
 	writer.save()
 
 
