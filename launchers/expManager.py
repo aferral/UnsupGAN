51dc9ac937ab0c6690bc884d06a6ffb29a8f236c
andres <andreslago5@hotmail.com>
2017-05-28 19:57:38 -0300

    New experiment to measeure mode collapse READY. also refactoring blokeh plot

--- launchers/expManager.py
+++ launchers/expManager.py
@@ -1,10 +1,10 @@
-from launchers.run_exp import train
+#from launchers.run_exp import train
 import os
 import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("configFile", help="Where is the configFile")
-parser.add_argument("--train", help="Do training",action="store_true")
+#parser.add_argument("--train", help="Do training",action="store_true")
 parser.add_argument("--classExp", help="Do clasifyExp",action="store_true")
 parser.add_argument("--discr", help="Do discriminator exp",action="store_true")
 parser.add_argument("--modeCollapse", help="Do modeCollapse exp",action="store_true")
@@ -15,7 +15,7 @@ configFilePath = args.configFile
 print "About to use ",configFilePath
 if args.train:
     print "train turned on"
-    train(configFilePath)
+    #train(configFilePath)
 if args.classExp:
     print "clasifyExp turned on"
     os.system("python -m launchers.clasifyExp"+ " " + configFilePath)
