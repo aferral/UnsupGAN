4a195854caf4d876f9c8fcd8d962fb4e0f433c78
andres <andreslago5@hotmail.com>
2017-07-15 11:37:09 -0300

    Testing resize conv instead of deconv for improved arch

--- infogan/misc/custom_ops.py
+++ infogan/misc/custom_ops.py
@@ -102,9 +102,8 @@ class custom_deconv2d(pt.VarStoreMethod):
 
             if useResize:
                 upsample = tf.image.resize_images(input_layer,output_shape[1],output_shape[2],1)
-                conv = custom_conv2d()
                 #Setting pad to 1,1 to keep dimensions
-                convUpsample = conv(upsample, upsample.shape,
+                convUpsample = custom_conv2d(upsample, upsample.shape,
                                                       k_h=k_h, k_w=k_w, d_h=1, d_w=1, stddev=stddev, in_dim=None,
                                                       padding='SAME',
                                                       name="conv2d")
